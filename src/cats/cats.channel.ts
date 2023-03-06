d0ad783d54491612a7ff6d77487ae82c60fde961
magocod <magoyeison@gmail.com>
2023-03-06 19:52:59 -0400

    views channel

--- src/cats/cats.channel.ts
+++ src/cats/cats.channel.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { RabbitmqChannel } from '../rabbitmq/rabbitmq.channel';
 import { Cat, CatDocument } from './entities';
 import { catTasks } from './cats.constants';
@@ -9,12 +9,23 @@ import { CreateCatDto } from './dto';
 
 Injectable();
 export class CatsChannel extends RabbitmqChannel<Cat> {
+  public logger = new Logger('CatsChannel');
   constructor(@InjectModel(Cat.name) private catModel: Model<CatDocument>) {
     super(catTasks);
   }
 
   async process(payload: ConsumeMessage): Promise<Cat> {
     const createCatDto: CreateCatDto = JSON.parse(payload.content.toString());
+    // throw new Error('example error in process');
     return this.catModel.create(createCatDto);
   }
+
+  async onSuccess(result: Cat) {
+    // throw new Error('example error in onSuccess');
+    this.logger.log(result);
+  }
+
+  // async onError(error: Error) {
+  //
+  // }
 }
--- src/rabbitmq/rabbitmq.channel.ts
+++ src/rabbitmq/rabbitmq.channel.ts
@@ -1,14 +1,27 @@
 import { Channel, ConsumeMessage } from 'amqplib';
 import { ChannelConsumer } from './interfaces';
+import { Logger } from '@nestjs/common';
 
 export abstract class RabbitmqChannel<T = unknown>
   implements ChannelConsumer<T, ConsumeMessage>
 {
+  public logger = new Logger('RabbitmqChannel');
+
   protected constructor(private readonly queueName: string) {}
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async process(payload: ConsumeMessage): Promise<T> {
     throw new Error(`${this.queueName} process not implemented`);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async onSuccess(result: T) {
+    // pass
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async onError(error: Error) {
+    this.logger.error(error);
   }
 
   async boot(ch: Channel): Promise<void> {
@@ -17,17 +30,29 @@ export abstract class RabbitmqChannel<T = unknown>
     });
     await ch.consume(this.queueName, (msg) => {
       if (msg !== null) {
-        console.log(`${this.queueName} Received: ${msg.content.toString()}`);
+        // console.log(`${this.queueName} Received: ${msg.content.toString()}`);
         this.process(msg)
           .then((result) => {
-            console.log(`${this.queueName} success: ${result}`);
+            // console.log(`${this.queueName} success: ${result}`);
+            this.onSuccess(result).catch((e) => {
+              this.logger.error(
+                `${this.queueName} onSuccess threw exception ${e}`,
+              );
+            });
             ch.ack(msg);
           })
+
           .catch((e) => {
-            console.log(`${this.queueName} error: ${e}`);
+            this.onError(e).catch((e) => {
+              this.logger.error(
+                `${this.queueName} onError threw exception ${e}`,
+              );
+            });
+            // console.log(`${this.queueName} error: ${e}`);
           });
       } else {
-        console.log(`${this.queueName} Consumer cancelled by server`);
+        // console.log(`${this.queueName} Consumer cancelled by server`);
+        this.logger.error(`${this.queueName} Consumer cancelled by server`);
       }
     });
   }
